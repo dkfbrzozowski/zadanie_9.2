b2590c46cfcd349ff25bddd8696f9f3312e83694
Dawid Brzozowski <d.brzozowski@wp.pl>
2017-11-29 12:10:15 +0100

    Modify script

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
-function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane');
-	} else {
-		return a * h / 2;
-	}
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+	allNames.push('Marian');
 }
-getTriangleArea();
-
-var triangle1Area = getTriangleArea(20, 5),
-	triangle2Area = getTriangleArea(10, 8),
-	triangle3Area = getTriangleArea(2, 5);
-
-console.log(triangle1Area);
-console.log(triangle2Area);
-console.log(triangle3Area);
+console.log(allNames);
